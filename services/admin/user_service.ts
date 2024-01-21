4cff8464bc4e62a22e0ff18301ad44b6101b0404
元 <13075035453@163.com>
2024-01-21 17:35:48 +0800

    test

--- services/admin/user_service.ts
+++ services/admin/user_service.ts
@@ -1,4 +1,9 @@
-
-export function GetUserinfo (username:string,password:string){
-    
+import DAO from '../../prisma/prisma'
+export async function GetUserinfo (username:string,password:string){
+    let dbRes = await DAO.user.findFirst({
+        where: {
+            name: username,
+        },
+    })
+    return dbRes
 }
